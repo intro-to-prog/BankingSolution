dde27102658a1bd25666917d4bfc7d398277d5b2
Jeffry Gonzalez <jeff_gonzalez@progressive.com>
2021-08-24 15:23:27 -0400

    intial for realz this time

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public class BankAccount
+    {
+        public decimal GetBalance()
+        {
+            return 5000; // "Sliming"
+        }
+    }
+}
